950b457c3ed61dba022613596841cd9930c70c74
Tim Schraepen <sch3lp@gmail.com>
2017-04-04 16:00:55 +0200

    elm-architecture

---
+++ src/Main.elm
@@ -0,0 +1,17 @@
+module Main exposing(..)
+
+import Html exposing (Html)
+
+import Model exposing (Model,initialModel)
+import Update exposing (update)
+import View exposing (view)
+import Subscription exposing (subscriptions, initialWindowSizeCommand)
+
+
+main : Program Never Model Subscription.Msg
+main =
+  Html.program
+  { init = (initialModel, initialWindowSizeCommand)
+  , update = update
+  , view = view
+  , subscriptions = subscriptions }
