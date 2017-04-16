8c0c2d84bc263f0be8fb86a9a62eaae22e264ddd
Rob Swartenbroekx <rob.swartenbroekx@gmail.com>
2017-04-16 18:48:05 +0200

    Every branch should have is own deployment environment

--- buildscripts/deploy.sh
+++ buildscripts/deploy.sh
@@ -8,8 +8,8 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Sch3lp/moobase-cmdr gh-pages 2> /dev/null
 
-mkdir -p gh-pages/app
-cp $HOME/build/Sch3lp/moobase-cmdr/index.html ./gh-pages/app/index.html
+mkdir -p gh-pages/app/$TRAVIS_BRANCH
+cp $HOME/build/Sch3lp/moobase-cmdr/index.html ./gh-pages/app/$TRAVIS_BRANCH/index.html
 cd gh-pages
 git add -A
 git commit -m "new deploy: $TRAVIS_BUILD_NUMBER" 
