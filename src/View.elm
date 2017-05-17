9f97a7ed9175d533ed495078910d7a78b26c99cd
Vincent Verhoeven <verhoevenv@gmail.com>
2017-05-17 22:00:22 +0200

    Round force in view, we're moving to something visual eventually anyway

--- src/View.elm
+++ src/View.elm
@@ -84,7 +84,7 @@ view model =
             , button [onClick AimRight]  [Html.text "Aim right"]
             , span [] [ Html.text "Force: " ]
             , button [ onClick DecrementForce ] [ Html.text "-" ]
-            , span [] [ Html.text (toString model.power.force) ]
+            , span [] [ Html.text (toString <| round <| model.power.force) ]
             , button [ onClick IncrementForce ] [ Html.text "+" ]
             ]
         , svg 
